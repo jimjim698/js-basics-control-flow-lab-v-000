b330dbd0fa60e297714258434e56b57857eb9e74
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-04 20:55:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function scuberGreetingForFeet(someValue){
 
 function ternaryCheckCity(city){
   let result
-  city = "NYC" ? result = "Ok, sounds good." : ""
+  city = "NYC" ? result = "Ok, sounds good." : "No"
   return result
 }
