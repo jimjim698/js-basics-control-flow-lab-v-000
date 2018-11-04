de089236e90fde1be6204344ef52cebe4228445f
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-04 20:40:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ function scuberGreetingForFeet(someValue){
   let result
   if (someValue < 400){
     result = "This one is on me!";
-    else if (someValue > 2000)
+    else if (someValue > 2000){
     result = ""
+    }
   }
   return result
 }
